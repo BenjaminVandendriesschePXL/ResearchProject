a56aeab40d7537400a219672de4656c6dbd0724b
BenjaminVandendriesschePXL <benjamin.vandendriessche@student.pxl.be>
2023-05-08 18:18:11 +0200

    Database ontkoppeld

--- src/main/java/pxl/be/researchproject/repository/MovieRepository.java
+++ src/main/java/pxl/be/researchproject/repository/MovieRepository.java
@@ -5,6 +5,33 @@ import org.springframework.stereotype.Repository;
 import pxl.be.researchproject.api.MovieDTO;
 import pxl.be.researchproject.domain.Movie;
 
+import java.util.*;
+
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public class MovieRepository {
+    private HashMap<Long,Movie> movies;
+
+    public MovieRepository(){
+        movies = new HashMap<>();
+    }
+    public List<Movie> findAll() {
+        return new ArrayList<Movie>(movies.values());
+    }
+
+    public Movie getReferenceById(Long movieId) {
+        return movies.get(movieId);
+    }
+
+    public Optional<Movie> findById(Long movieId) {
+        return Optional.ofNullable(movies.get(movieId));
+    }
+
+    public Movie save(Movie movie) {
+        movies.put(movie.getId(),movie);
+        return movie;
+    }
+
+    public void delete(Movie movie) {
+        movies.remove(movie.getId());
+    }
 }
